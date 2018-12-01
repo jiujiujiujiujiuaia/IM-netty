40c94c75347580ad741f3db10ccf14aeff0ffca3
jiujiuaia <jiujiuaia@gmail.com>
2018-12-01 12:40:43 +0800

    add:BrowserAndClient

---
+++ src/main/java/com/ycw/im/BrowserAndClient/Init.java
@@ -0,0 +1,22 @@
+package com.ycw.im.BrowserAndClient;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * @Author yuchunwei
+ */
+public class Init extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel channel) throws Exception {
+        channel.pipeline().addLast(new HttpServerCodec());
+        channel.pipeline().addLast(new ResponseHandler());
+    }
+
+    public static void main(String[] args) {
+
+    }
+}   
---
+++ src/main/java/com/ycw/im/BrowserAndClient/ResponseHandler.java
@@ -0,0 +1,37 @@
+package com.ycw.im.BrowserAndClient;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+import java.nio.charset.Charset;
+
+/**
+ * @Author yuchunwei
+ */
+public class ResponseHandler extends SimpleChannelInboundHandler<HttpObject> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject httpObject) throws Exception {
+        Channel channel = ctx.channel();
+        ByteBuf byteBuf =ctx.alloc().buffer();
+        byteBuf.writeBytes("Hello Netty ".getBytes(Charset.forName("UTF-8")));
+//        ByteBuf content = Unpooled.copiedBuffer("Hello netty~", CharsetUtil.UTF_8);
+        System.out.println(channel.remoteAddress());
+
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
+                HttpResponseStatus.OK,byteBuf);
+        response.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/plain");
+        response.headers().set(HttpHeaderNames.CONTENT_LENGTH,byteBuf.readableBytes());
+
+        ctx.writeAndFlush(response);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}   
